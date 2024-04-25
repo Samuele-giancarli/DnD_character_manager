83792292b1ac774e58f15353588e72f33961a717
Samuele-giancarli <samugianca@gmail.com>
2024-04-25 19:06:40 +0200

    added working characterList

--- characterList.php
+++ characterList.php
@@ -15,7 +15,6 @@
             background-color: #f8f9fa;
         }
         .card {
-            margin-top: 50px;
             background-color: #d2b48c;
             border: 1px solid #8b4513;
         }
@@ -35,45 +34,59 @@
 </head>
 <body>
 
-<header><nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">DND Aider</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-<!-- nella home vedo la lista dei miei personaggi e il pulsante "crea"-->
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="login.php">Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Create</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Guides</a>
-      </li>
-    </ul>
-  </div>
-</nav></header>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">DND Aider</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Create</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Guides</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
 
-    <?php 
-        if(!isset($_SESSION["ID"])){
-            header("Location: login.php");
-        } else {
-            foreach($dbh->characterList($_SESSION["ID"]) as $character){
-                echo "<li class='nav-item'>";
-                echo "<a class='nav-link' href='character.php?ID=".$character["ID_Personaggio"]."'>".$character["Nome"]."</a>";
-                echo "</li>";
+<div class="container">
+    <div class="row justify-content-center">
+        <?php 
+            if(!isset($_SESSION["ID"])){
+                header("Location: login.php");
+            } else {
+                foreach($dbh->characterList($_SESSION["ID"]) as $character){
+        ?>
+            <div class="col-md-4">
+                <div class="card mb-3">
+                    <div class="card-header"><?php echo $character["Nome"]; ?></div>
+                    <div class="card-body">
+                        <p class="card-text">Altre informazioni sul personaggio qui...</p>
+                        <a href="character.php?ID=<?php echo $character["ID_Personaggio"]; ?>" class="btn btn-primary">Dettagli</a>
+                    </div>
+                </div>
+            </div>
+        <?php
+                }
+                echo '<div class="col-md-4">';
+                echo '<a href="createCharacter.php" class="btn btn-primary btn-block">Crea Personaggio</a>';
+                echo '</div>';
             }
+        ?>
+    </div>
+</div>
 
-            echo '<a href="createCharacter.php" class="btn btn-primary">Crea Personaggio</a>';
-        }
-    ?>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 
 </body>
 </html>
