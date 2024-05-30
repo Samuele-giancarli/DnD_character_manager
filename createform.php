0f39975dabf0fccf2dafd96bc7f649024142d903
Palikko <93582735+Palikko@users.noreply.github.com>
2024-05-30 12:05:02 +0200

    Update createform.php

--- createform.php
+++ createform.php
@@ -104,10 +104,10 @@ if ($myvalues==$reference){
 
         $nomeabilita=$singolabilita["Nome"];
         if (!is_null($singolabilita["Nome_Classe"])){
-            $valore+=1; //ToDo:modificare 1 con bonus competenza della classa (se presenti più classi prendere il maggiore)
+            $valore+=2; 
         }
         if (!is_null($singolabilita["Nome_Origine"])){
-            $valore+=1; //ToDo:modificare 1 con bonus competenza della classa (se presenti più classi prendere il maggiore)
+            $valore+=2;
         }
     $dbh->updateAbilita($idpersonaggio, $nomeabilita, $valore);
     $valore=0;
